e3b8655e5ba497067eb2624be8c6e31fa720834e
Nick D'Angelo <nick.dangelo@gmail.com>
2018-05-15 04:48:53 -0500

    Chapter 8 Content

---
+++ Chapter_08/8-1 message.py
@@ -0,0 +1,5 @@
+def display_message():
+    message = "Learning about Functions!"
+    print(message)
+
+display_message()
---
+++ Chapter_08/greeter.py
@@ -0,0 +1,12 @@
+def greet_user():
+    """Display a simple greeting."""
+    print("Hello!")
+
+greet_user()
+
+# Passing Information to a Function
+def greet_user(username):
+    """Display a simple greeting."""
+    print("Hello, " + username.title() + "!")
+
+greet_user('jesse')
